b9e7edf7b7ba68b9fe5d5e7c6c5e58c226df0bdc
Tonny Oseko <tonnyoseko0@gmail.com>
2023-10-29 23:49:34 +0300

    Create a Restful API
    Created to perform CRUD operations

---
+++ src/main/java/com/tonnyseko/crud/crudapp/user/UserController.java
@@ -0,0 +1,50 @@
+package com.tonnyseko.crud.crudapp.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/users")
+public class UserController {
+//    provide an instance of the user repository
+    @Autowired
+    private UserRepository userRepository;
+
+//    create a method to get all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).get();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user){
+        User existingUser = userRepository.findById(id).get();
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+        return userRepository.save(existingUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id){
+        try {
+            userRepository.findById(id).get();
+            userRepository.deleteById(id);
+            return "User deleted successfully";
+        } catch(Exception e){
+            return "User not found";
+        }
+    }
+    
+}
