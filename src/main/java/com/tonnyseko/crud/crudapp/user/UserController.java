61f983cee9b8fa50c8fe3948105267697c323c55
Tonny Oseko <tonnyoseko0@gmail.com>
2023-10-30 11:37:38 +0300

    add a .gitignore file

--- src/main/java/com/tonnyseko/crud/crudapp/user/UserController.java
+++ src/main/java/com/tonnyseko/crud/crudapp/user/UserController.java
@@ -1,7 +1,5 @@
 package com.tonnyseko.crud.crudapp.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
--- src/main/java/com/tonnyseko/crud/crudapp/user/UserRepository.java
+++ src/main/java/com/tonnyseko/crud/crudapp/user/UserRepository.java
@@ -1,8 +1,5 @@
 package com.tonnyseko.crud.crudapp.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
