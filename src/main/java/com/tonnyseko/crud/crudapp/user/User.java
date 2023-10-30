90baa61c9f45a03b642bb83145b832cf10be7fdc
Tonny Oseko <tonnyoseko0@gmail.com>
2023-10-30 12:14:58 +0300

    refactor project dependencies, add .gitignore file

--- src/main/java/com/tonnyseko/crud/crudapp/user/User.java
+++ src/main/java/com/tonnyseko/crud/crudapp/user/User.java
@@ -1,5 +1,12 @@
 package com.tonnyseko.crud.crudapp.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "users")
 public class User {
--- src/main/java/com/tonnyseko/crud/crudapp/user/UserRepository.java
+++ src/main/java/com/tonnyseko/crud/crudapp/user/UserRepository.java
@@ -1,5 +1,8 @@
 package com.tonnyseko.crud.crudapp.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
